6e3083112db1adb53507b2a90ead63b1ed060ef7
Alanis945 <luis090405@hotmail.com>
2025-02-20 01:26:33 -0600

    Add files via upload

---
+++ practica 5 sistema de calificaciones/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace practica_5_sistema_de_calificaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.btnSiguente = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblCalF = new System.Windows.Forms.Label();
+            this.lblAR = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblEstatus = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.lblNumeroFinal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido al Sistema de Evaluacion UDM.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(391, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Por favor, ingrese la siguiente información requerida:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(400, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(388, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(87, 58);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(277, 20);
+            this.txtNom.TabIndex = 5;
+            this.txtNom.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Primer Parcial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(127, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Segundo Parcial";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(258, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Tercer Parcial";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(15, 102);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(106, 20);
+            this.txt1.TabIndex = 9;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(130, 102);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(122, 20);
+            this.txt2.TabIndex = 10;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(258, 102);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(106, 20);
+            this.txt3.TabIndex = 11;
+            this.txt3.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // btnSiguente
+            // 
+            this.btnSiguente.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguente.Location = new System.Drawing.Point(6, 137);
+            this.btnSiguente.Name = "btnSiguente";
+            this.btnSiguente.Size = new System.Drawing.Size(586, 23);
+            this.btnSiguente.TabIndex = 12;
+            this.btnSiguente.Text = "SIGUIENTE";
+            this.btnSiguente.UseVisualStyleBackColor = true;
+            this.btnSiguente.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(6, 166);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(586, 88);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(12, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 172);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 18);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "El/La Estudiante";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.White;
+            this.lblNombre.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(143, 172);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(20, 18);
+            this.lblNombre.TabIndex = 16;
+            this.lblNombre.Text = "///";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.White;
+            this.label10.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 206);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(280, 18);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Por Optener una Calificalcion final de:";
+            // 
+            // lblCalF
+            // 
+            this.lblCalF.AutoSize = true;
+            this.lblCalF.BackColor = System.Drawing.Color.White;
+            this.lblCalF.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalF.Location = new System.Drawing.Point(282, 206);
+            this.lblCalF.Name = "lblCalF";
+            this.lblCalF.Size = new System.Drawing.Size(20, 18);
+            this.lblCalF.TabIndex = 18;
+            this.lblCalF.Text = "///";
+            // 
+            // lblAR
+            // 
+            this.lblAR.AutoSize = true;
+            this.lblAR.BackColor = System.Drawing.Color.White;
+            this.lblAR.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAR.Location = new System.Drawing.Point(12, 190);
+            this.lblAR.Name = "lblAR";
+            this.lblAR.Size = new System.Drawing.Size(20, 18);
+            this.lblAR.TabIndex = 19;
+            this.lblAR.Text = "///";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.White;
+            this.label13.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(12, 224);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(117, 18);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Su Estatu sera:";
+            // 
+            // lblEstatus
+            // 
+            this.lblEstatus.AutoSize = true;
+            this.lblEstatus.BackColor = System.Drawing.Color.White;
+            this.lblEstatus.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstatus.Location = new System.Drawing.Point(127, 224);
+            this.lblEstatus.Name = "lblEstatus";
+            this.lblEstatus.Size = new System.Drawing.Size(20, 18);
+            this.lblEstatus.TabIndex = 21;
+            this.lblEstatus.Text = "///";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.White;
+            this.label15.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(451, 172);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(132, 18);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "Calificacion Final";
+            // 
+            // lblNumeroFinal
+            // 
+            this.lblNumeroFinal.AutoSize = true;
+            this.lblNumeroFinal.BackColor = System.Drawing.Color.White;
+            this.lblNumeroFinal.Font = new System.Drawing.Font("Arial Black", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroFinal.Location = new System.Drawing.Point(466, 190);
+            this.lblNumeroFinal.Name = "lblNumeroFinal";
+            this.lblNumeroFinal.Size = new System.Drawing.Size(105, 56);
+            this.lblNumeroFinal.TabIndex = 23;
+            this.lblNumeroFinal.Text = "000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(599, 262);
+            this.Controls.Add(this.lblNumeroFinal);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.lblEstatus);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblAR);
+            this.Controls.Add(this.lblCalF);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnSiguente);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button btnSiguente;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblCalF;
+        private System.Windows.Forms.Label lblAR;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblEstatus;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label lblNumeroFinal;
+    }
+}
+
---
+++ practica 5 sistema de calificaciones/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_5_sistema_de_calificaciones
+{
+    public partial class Form1 : Form
+    {
+        Decimal CalFin;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // traslada el nombre del estudiante 
+            lblNombre.Text = txtNom.Text;
+            txtNom.Text = "";
+
+        try
+        { 
+            // Convertir el contenido de cada TextBox a decimal
+            decimal cal1 = Convert.ToDecimal(txt1.Text);
+            decimal cal2 = Convert.ToDecimal(txt2.Text);
+            decimal cal3 = Convert.ToDecimal(txt3.Text);
+
+            // Calcular la calificacion final
+            decimal CalFin = (cal1 + cal2 + cal3) / 3;
+
+            // Se muestra el resultado con un decimal (F1 formatea a 1 cifra decimal)
+            lblCalF.Text = CalFin.ToString("F1");
+            lblNumeroFinal.Text = CalFin.ToString("F1");
+
+            // Mostrar si Aprovo o Reprobo
+            if (CalFin >= 7.0m) // Aprobado si es mayor o Igual a 7.0
+            {
+                lblAR.Text = "¡¡Aprobo!! "; //Aprobo
+                    //Par cambiar el color de la letra utilizaremos ForeColor
+                lblAR.ForeColor = Color.Green;  // Texto en Verde para Aprobar
+                lblCalF.ForeColor = Color.Green; // Texto en Verde para el Promedio Alto
+                lblNumeroFinal.ForeColor = Color.Green; //Texto en Verde para la Calificacion Final
+            }
+            else
+            {
+                lblAR.Text = "¡¡Reprobo!! "; //Reprobo
+                lblAR.ForeColor = Color.Red;   // Texto en Rojo para Reprobar
+                lblCalF.ForeColor = Color.Red; // Texto en Rojo para el Promedio Bajo
+                lblNumeroFinal.ForeColor = Color.Red; //Texto en Rojo para la Calificacion Final
+            }
+
+                // Clasificar según el promedio y cambiar el color del label de categoría
+                if (CalFin >= 9.5m)
+                {
+                    lblEstatus.Text = "Exento ";
+                    lblEstatus.ForeColor = Color.Green; // Verde para Exento
+                }
+                else if (CalFin >= 7.0m && CalFin <= 9.4m)
+                {
+                    lblEstatus.Text = "Ordinario ";
+                    lblEstatus.ForeColor = Color.Yellow; // Amarillo para Ordinario
+                }
+                else if (CalFin >= 5.0m && CalFin <= 6.9m)
+                {
+                    lblEstatus.Text = "Extraordinario ";
+                    lblEstatus.ForeColor = Color.Orange; // Naranja para Extraordinario
+                }
+                else if (CalFin < 5.0m)
+                {
+                    lblEstatus.Text = "Especial";
+                    lblEstatus.ForeColor = Color.Red;    // Rojo para Especial
+                }
+                //uso el sufijo m para especificar que el número es decimal y evitar problemas de conversión de tipos.
+            }
+            catch (Exception ex)
+    {
+        // En caso de que ingresen mal el dato les aparecera Error, se muestra el mensaje en lblAR
+        lblAR.Text = "Error" + ex.Message;
+        lblAR.ForeColor = Color.Black; // Color por defecto para errores
+        lblCalF.ForeColor = Color.Black; // Color por defecto para el promedio
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
