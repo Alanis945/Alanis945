f36588a0c81c82eab88e9cb9d00ff6bc04ecc907
Alanis945 <luis090405@hotmail.com>
2025-05-19 10:05:23 -0500

    Add files via upload

---
+++ Suma.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+    // Definimos las variable 
+    $num1 = 8;
+    $num2 = 12;
+
+    // Realiza la suma 
+    $resultado = $num1 + $num2;
+
+    // Muestra el resultado 
+    echo "<h1>RESULTADO DE LA SUMA</h1>";
+    echo "<p>La suma de <strong>$num1</strong> + <strong>$num2</strong> es <strong>$resultado</strong>.</p>";
+    ?>
+</body>
+</html>
